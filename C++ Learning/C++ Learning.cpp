b5243252af5201a60e3cc1fa4d1424e42935b68a
Alex Ang <zi_xuan1431@msn.com>
2021-07-02 20:34:35 +0800

    stop at switch case lesson

--- C++ Learning/C++ Learning.cpp
+++ C++ Learning/C++ Learning.cpp
@@ -1,5 +1,8 @@
 // C++ Learning.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+// Tutorial learnt from: (Youtube) https://www.youtube.com/watch?v=vLnPwxZdW4Y
+// Author: Alex Ang
+// Year: 2021
 
 #include <iostream>
 #include <string>   // need to include this to use string without interruption
@@ -7,22 +10,27 @@
 
 using namespace std;
 
-int main()
+void variableAndDataType()
 {
 	string myName = "Alexander";
 	int myAge;
 	myAge = 100;
 
-	// variables
-    cout << "Hello World and Hello " << myName << endl;
+	cout << "Hello World and Hello " << myName << endl;
 	cout << "I am " << myAge << " years old." << endl;
+}
 
-	// string functionc
+void stringFunction()
+{
+	string myName = "Alexander";
+
 	cout << "Length of my name: " << myName.length() << endl;
 	cout << "Where X located from index 0: " << myName.find("x", 0) << endl;
 	cout << "Substring start from index 2: " << myName.substr(2, 2) << endl;
+}
 
-	// basic cmath function
+void basicCMathFunction()
+{
 	cout << pow(2, 2) << endl;
 	cout << sqrt(36) << endl;
 	cout << round(4.5) << endl;	// normal rounding rule
@@ -30,8 +38,10 @@ int main()
 	cout << floor(5.9) << endl; // rounded to the lowest number before the current one
 	cout << fmax(2, 10) << endl;
 	cout << fmin(2, 10) << endl;
+}
 
-	// user input
+void userInput()
+{
 	string userInput;
 
 	cout << "Enter something in string: \n";
@@ -47,17 +57,95 @@ int main()
 	cout << "A line of everything you input: \n";
 	getline(cin, userInput);
 	cout << "\nSomething you input: " << userInput << endl;
+}
+
+void SimpleArray()
+{
+	int luckyNo[] = { 1,2,3,4,5 };
+
+	string stringList[10];
+
+	stringList[0] = "aSDasdad";
+
+	cout << stringList[0];
+}
+
+// C++ functions must be above main unless function prototyping is used where function name with type and parameter/s is declared above main function
+
+void simpleFunction(string name);
+double cube(double num);
+void ifStatement(bool gay, bool happy, bool crazy);
+void switchCase(int numberToDayOfWeek);
+
+int main()
+{
+	switchCase(1);
 
 	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void simpleFunction(string name = "")
+{
+	cout << "Yo! " << name << endl;
+}
 
-// Tips for Getting Started:
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+double cube(double num)
+{
+	return pow(num, 3);
+}
+
+void ifStatement(bool gay, bool happy, bool crazy)
+{
+	bool isGay = gay;
+	bool isHappy = happy;
+	bool isCrazy = crazy;
+
+	if (isGay && isHappy)
+	{
+		cout << "Yo good man";
+	}
+	else if (isCrazy)
+	{
+		cout << "Ya crazy man";
+	}
+	else
+	{
+		cout << "Nothing Happen" << endl;
+	}
+}
+
+void switchCase(int numberToDayOfWeek)
+{
+	string dayOfWeek;
+
+	switch (numberToDayOfWeek)
+	{
+		case 0:
+			dayOfWeek = "Sunday";
+			break;
+		case 1:
+			dayOfWeek = "Monday";
+			break;
+		case 2:
+			dayOfWeek = "Tuesday";
+			break;
+		case 3:
+			dayOfWeek = "Wednesday";
+			break;
+		case 4:
+			dayOfWeek = "Thursday";
+			break;
+		case 5:
+			dayOfWeek = "Friday";
+			break;
+		case 6:
+			dayOfWeek = "Saturday";
+			break;
+		default:
+			dayOfWeek = "Invalid Day";
+			break;
+	}
+
+	cout << dayOfWeek << endl;
+}
+
