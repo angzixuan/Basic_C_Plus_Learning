ad02e9080ffd936aecee7d7af268b80f5d6ee6d2
Alex Ang <zi_xuan1431@msn.com>
2021-07-05 22:50:14 +0800

    finish basic and plan to advance

--- C++ Learning/C++ Learning.cpp
+++ C++ Learning/C++ Learning.cpp
@@ -1,6 +1,6 @@
 // C++ Learning.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-// Tutorial learnt from: (Youtube) https://www.youtube.com/watch?v=vLnPwxZdW4Y
+// Tutorial learn from: (Youtube) https://www.youtube.com/watch?v=vLnPwxZdW4Y
 // Author: Alex Ang
 // Year: 2021
 
@@ -80,10 +80,99 @@ void whileLoop();
 void doWhileLoop();
 void guessANumber();
 void forLoop();
+void power(int baseNum, int powNum);
+void twoDArray();
+void pointers();
+
+// class
+class Book {
+
+// access modifier
+private:
+	string title;
+	string author;
+	int pages;
+
+	void bookCreated() {
+		cout << "Book, " << title << " was written by " << author << " with " << pages << " pages." << endl;
+	}
+
+public:
+		// constructor and overloaded
+		Book() {
+			title = "";
+			author = "";
+			pages = 0;
+		}
+
+		Book(string aTitle = "", string aAuthor="", int aPages=0) {
+			title = aTitle;
+			author = aAuthor;
+			pages = aPages;
+
+			bookCreated();
+			hasAuthor();
+
+		}
+
+		void hasAuthor() {
+			if (author != "") {
+				cout << "There is an author named, " << author << endl;
+			}
+			else {
+				cout << "There is no author" << endl;
+			}
+		}
+
+		void setAuthor(string aAuthor) {
+			author = aAuthor;
+		}
+
+		string getAuthor() {
+			return author;
+		}
+};
+
+class Textbook : public Book {
+
+public:
+	// inherit Book constructor
+	Textbook(string aTitle = "", string aAuthor = "", int aPages = 0) : Book(aTitle , aAuthor , aPages ) {
+
+	}
+
+	// override the function in base class
+	void hasAuthor() {
+		if (getAuthor() != "") {
+			cout << "There is an author named, " << getAuthor() << "!!!" << endl;
+		}
+		else {
+			cout << "There is no author!!!" << endl;
+		}
+	}
+};
+
+void testBookClass()
+{
+	Book book1("Happy Tree Friends", "Friends of Trees", 100);
+	Book book2();
+
+	cout << book1.getAuthor();
+}
+
+void testBookClass2()
+{
+	// hasAuthor in base constructor will be trigger from the base class instead
+	Textbook Biology("Happy", "Tree", 100);
+
+	// this will trigger from Textbook class
+	Biology.hasAuthor();
+
+}
 
 int main()
 {
-	forLoop();
+	testBookClass2();
 
 	return 0;
 }
@@ -203,5 +292,63 @@ void guessANumber()
 
 void forLoop()
 {
+	int index = 0;
+	int nums[] = { 1,2,3,4,5 };
+	int len = sizeof(nums) / sizeof(nums[0]);
 
+	for (index; index <= 10; index++) {
+		cout << "Index: " << index << endl;
+	}
+
+	cout << "length of array: " << len << endl;
+
+	for (int i = 0; i < len ; i++) {
+		cout << nums[i] << endl;
+	}
+}
+
+void power(int baseNum, int powNum)
+{
+	int result = 1;
+
+	for (int i = 0; i < powNum; i++) {
+		result = result * baseNum;
+	}
+
+	cout << "Result of power " << powNum << " of " << baseNum << " is " << result << endl;
+}
+
+void twoDArray()
+{
+	int number2d[][2] = {
+		{1, 2},
+		{3, 4},
+	};
+
+	int lenRow = sizeof(number2d) / sizeof(number2d[0]);
+	int lenCol = sizeof(number2d[0]) / sizeof(int);
+
+	cout << "length of row: " << lenRow << endl;
+	cout << "length of column: " << lenCol << endl;
+
+	cout << "Testing " << number2d[0][0] << endl;
+
+	for (int i = 0; i < lenRow; i++) {
+		for (int j = 0; j < lenCol; j++) {
+			cout << number2d[i][j] << endl;
+		}
+	}
+}
+
+void pointers()
+{
+	// 100 is stored in physical memory, age is storing the memory address
+	int age = 100;
+	// pointer variable
+	int *pAge = &age;
+
+	// this is a pointer (address) to the physical memory
+	cout << &age << endl;
+	// dereferencing a pointer is grabbing value in that physical address
+	cout << *pAge << endl;
 }
